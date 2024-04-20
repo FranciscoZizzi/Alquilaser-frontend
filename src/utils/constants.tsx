27f476395d0cc58bc0be68bd4a89ee06d8f0962a
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-04-20 00:30:05 -0300

    fixed addListing URL

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,5 +1,5 @@
 export const BASE_URL = 'http://localhost'
 export const SEARCH_PATH = '/api/search'
 
-export const ADD_LISTING_PATH = '/api/listing/add'
+export const ADD_LISTING_PATH = '/api/listings/add'
 export const PORT = '3001'
