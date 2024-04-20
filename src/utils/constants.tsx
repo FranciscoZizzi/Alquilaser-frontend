d3d1a9e95b9aa4f980141826f1096c8b4a468a0e
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-04-20 00:22:53 -0300

    getAddListingUrl() method

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,3 +1,5 @@
 export const BASE_URL = 'http://localhost'
 export const SEARCH_PATH = '/api/search'
+
+export const ADD_LISTING_PATH = '/api/listing/add'
 export const PORT = '3001'
--- src/utils/url.tsx
+++ src/utils/url.tsx
@@ -1,5 +1,9 @@
-import {BASE_URL, PORT, SEARCH_PATH} from "./constants";
+import {ADD_LISTING_PATH, BASE_URL, PORT, SEARCH_PATH} from "./constants";
 
 export function getSearchURL() {
     return BASE_URL + ':' + PORT + SEARCH_PATH
+}
+
+export function getAddListingURL() {
+    return BASE_URL + ':' + PORT + ADD_LISTING_PATH
 }
