a536c0654a2817c5644160ca6eb0f91fc8668d39
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-04-21 15:26:00 -0300

    finished calendar functionality

--- src/components/calendar/BookingCalendar.tsx
+++ src/components/calendar/BookingCalendar.tsx
@@ -4,13 +4,26 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import 'dayjs/locale/en-gb';
 import dayjs, {Dayjs} from "dayjs";
 import {DateRange} from "@mui/icons-material";
+import axios from "axios";
+import {BASE_URL, PORT} from "../../utils/constants";
 
 type DateRange = {from: Dayjs, to: Dayjs}
 
 const BookingCalendar = ({listingId, maxBookDuration}:{listingId: string | undefined, maxBookDuration: number}) => {
     const [startDate, setStartDate] = useState<Dayjs | null>();
     const [endDate, setEndDate] = useState<Dayjs | null>();
-    const [bookedDates, setBookedDate] = useState<DateRange[]>([]);
+    const [bookedDates, setBookedDates] = useState<DateRange[]>([]);
+
+    useEffect(() => {
+        axios.get(BASE_URL + ':' + PORT + `/api/listings/bookings/${listingId}`)
+            .then(res => {
+                let bookings = res.data;
+                let dates: DateRange[] = []
+                bookings.forEach((booking: any) => dates.push({from: dayjs(booking.start_date), to: dayjs(booking.end_date)}))
+                setBookedDates(dates);
+            })
+            .catch((e: any) => alert(e.response.data));
+    }, []);
 
     const dateIsBooked = (date: dayjs.Dayjs | null | undefined) => {
         if (!date) return false;
@@ -21,7 +34,7 @@ const BookingCalendar = ({listingId, maxBookDuration}:{listingId: string | undef
 
     const dateIsDisabled = (date: Dayjs) => {
         if (!date) return false;
-        if (!startDate) return false;
+        if (!startDate) return true;
         let nextBookedDate = getNextBookedDate(startDate);
         return date < startDate || date >= nextBookedDate;
     }
@@ -50,5 +63,3 @@ const BookingCalendar = ({listingId, maxBookDuration}:{listingId: string | undef
     )
 }
 export default BookingCalendar;
-
-// https://mui.com/x/react-date-pickers/date-calendar/
