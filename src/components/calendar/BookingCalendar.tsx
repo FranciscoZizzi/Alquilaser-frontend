e23ebea6f205b2885bee2a0afdb1b6c5ec988c31
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-04-21 14:36:32 -0300

    DatePickers disable booked dates

--- src/components/calendar/BookingCalendar.tsx
+++ src/components/calendar/BookingCalendar.tsx
@@ -1,34 +1,49 @@
 import React, {useEffect, useState} from "react"
 import {DatePicker, LocalizationProvider} from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import 'dayjs/locale/en-gb';
 import dayjs, {Dayjs} from "dayjs";
+import {DateRange} from "@mui/icons-material";
 
 type DateRange = {from: Dayjs, to: Dayjs}
 
-const BookingCalendar = () => {
+const BookingCalendar = ({listingId, maxBookDuration}:{listingId: string | undefined, maxBookDuration: number}) => {
     const [startDate, setStartDate] = useState<Dayjs | null>();
     const [endDate, setEndDate] = useState<Dayjs | null>();
-    const [bookedDates, setBookedDate] = useState<DateRange[]>([
-        // {from: dayjs(), to:dayjs('2024-05-15')}
-    ]);
-    const [disabledDates, setDisabledDate] = useState<DateRange[]>([]);
+    const [bookedDates, setBookedDate] = useState<DateRange[]>([]);
 
-    const dateIsBooked = (date: Dayjs) => {
+    const dateIsBooked = (date: dayjs.Dayjs | null | undefined) => {
+        if (!date) return false;
         return bookedDates.some((range) => {
             return date >= range.from && date <= range.to
         })
     }
 
     const dateIsDisabled = (date: Dayjs) => {
-        return disabledDates.some((range) => {
-            return date >= range.from && date <= range.to
-        })
+        if (!date) return false;
+        if (!startDate) return false;
+        let nextBookedDate = getNextBookedDate(startDate);
+        return date < startDate || date >= nextBookedDate;
+    }
+
+    const getNextBookedDate = (from: dayjs.Dayjs) => {
+        let date = from;
+        let counter = 0;
+        while (!dateIsBooked(date) && counter < maxBookDuration) {
+            date = date.add(1, 'day');
+            counter++;
+        }
+        return date;
+    }
+
+    const handleStartDateChange = (e: any) => {
+        setStartDate(e);
     }
 
     return(
         <div>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker label="From" value={startDate} onChange={setStartDate} shouldDisableDate={dateIsBooked} minDate={dayjs()}/>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='en-gb'>
+                <DatePicker label="From" value={startDate} onChange={handleStartDateChange} shouldDisableDate={dateIsBooked} minDate={dayjs()}/>
                 <DatePicker label="To" value={endDate} onChange={setEndDate} shouldDisableDate={dateIsDisabled} minDate={dayjs()}/>
             </LocalizationProvider>
         </div>
