9b73fe916a4c8570288badef34622412523af538
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-06-10 17:50:09 -0300

    changed dropdown

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -4,7 +4,6 @@ import './dropDownStyle.css';
 
 type DropDownProps = {
     options: any[],
-    value: any,
     showDropDown: boolean;
     toggleDropDown: Function;
     onChange: (event: any) => void
@@ -15,9 +14,6 @@ const Dropdown: React.FC<DropDownProps> = ({   options,
 }: DropDownProps): React.JSX.Element => {
     const [showDropDown, setShowDropDown] = useState<boolean>(false);
 
-    const handleChange = (e: any) => {
-        onChange(e.target.value);
-    }
     useEffect(() => {
         setShowDropDown(showDropDown)
     }, [showDropDown]);
@@ -25,6 +21,9 @@ const Dropdown: React.FC<DropDownProps> = ({   options,
     let optionComponents: any[] = [];
     options.forEach((option: string) => optionComponents.push(<option>{option}</option>))
 
+    const onClickHandler = (value:any) =>{
+        onChange(value)
+    }
     return (
         <div className={showDropDown ? 'dropdown' : 'dropdown active'}>
             {options.map(
@@ -33,25 +32,14 @@ const Dropdown: React.FC<DropDownProps> = ({   options,
                         <p
                             key={index}
                             onClick={(): void =>{
-                                handleChange(value)
+                                onClickHandler(value)
                         }}
                         >
                             {value}
                         </p>
                     )
                 }
-            )
-            }
-            {/*<select style={{*/}
-            {/*    width: 'calc(100% - 0.4rem - 4px)',*/}
-            {/*    padding: '1rem',*/}
-            {/*    position: 'relative',*/}
-            {/*    borderWidth: '2px',*/}
-            {/*    borderColor: '#0167f8',*/}
-            {/*    borderRadius: '0.375rem'*/}
-            {/*}} value={value} onChange={handleChange}>*/}
-            {/*    {optionComponents}*/}
-            {/*</select>*/}
+            )}
         </div>
     )
 }
