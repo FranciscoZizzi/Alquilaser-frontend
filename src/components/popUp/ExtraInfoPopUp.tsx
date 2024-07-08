4c3478f489176bf8ca84e3983fb28fadb47e7238
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-07-08 19:16:28 -0300

    added cancel booking button on ExtraInfoPopUp

--- src/components/popUp/ExtraInfoPopUp.tsx
+++ src/components/popUp/ExtraInfoPopUp.tsx
@@ -3,6 +3,9 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './popUpStyle.css'
 import Button from "../button/Button";
+import axios from "axios";
+import {BASE_URL, PORT} from "../../utils/constants";
+import {toast, ToastContainer} from "react-toastify";
 
 interface ExtraInfoPopUpProps{
     title: string;
@@ -13,10 +16,24 @@ interface ExtraInfoPopUpProps{
     dateOfReservation: string;
     prevDamage: string;
     additionalDamage?: string;
+    isClient: boolean;
+    bookingId: number
 }
 
-const ExtraInfoPopUp: React.FC<ExtraInfoPopUpProps> = ({title, client , owner, rate,finalPrice, dateOfReservation,prevDamage, additionalDamage}) => {
+const ExtraInfoPopUp: React.FC<ExtraInfoPopUpProps> = ({title, client , owner, rate,finalPrice, dateOfReservation,prevDamage, additionalDamage, isClient, bookingId}) => {
     const [open, setOpen] = useState(false);
+    const handleCancelButton = () => {
+        axios.delete(BASE_URL + ':' + PORT + `/api/bookings/delete/${bookingId}`).then((res) => {
+            window.location.reload();
+            toast(res.data.message);
+            }
+        ).catch((e) => {
+            toast("alo")
+            console.log(e)
+            console.log("message: " + e.response.data)
+            toast(e.response.data.message)
+        });
+    }
 
     return (
         <Popup
@@ -26,7 +43,8 @@ const ExtraInfoPopUp: React.FC<ExtraInfoPopUpProps> = ({title, client , owner, r
             contentStyle={{
                 borderRadius: '10px', // Apply rounded corners to the popup content
             }}
-            trigger={<Button variant={"empty"} style={{width:140, height:40}} className="button" onClick={() => setOpen(!open)}>Additional info</Button>}
+            trigger={<Button variant={"empty"} style={{width: 140, height: 40}} className="button"
+                             onClick={() => setOpen(!open)}>Additional info</Button>}
         >
 
             <div className="modal">
@@ -36,29 +54,32 @@ const ExtraInfoPopUp: React.FC<ExtraInfoPopUpProps> = ({title, client , owner, r
                 <h1 style={{
                     textAlign: "center"
                 }}>Additional info</h1>
-                <div style={{ display: 'flex', flexDirection: 'column', fontSize: '20px', gap:'20px' }}>
+                <div style={{display: 'flex', flexDirection: 'column', fontSize: '20px', gap: '20px'}}>
                     {/* First Row */}
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                        <div style={{ flexBasis: '50%' }}>{title}</div>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                        <div style={{flexBasis: '50%'}}>{title}</div>
                         {client ? (
-                            <div style={{ flexBasis: '50%' }}>Client: {client}</div>
+                            <div style={{flexBasis: '50%'}}>Client: {client}</div>
                         ) : (
-                            <div>
-                                <div style={{ flexBasis: '50%' }}>Owner: {owner}</div>
-                            </div>
+                                <div style={{flexBasis: '50%'}}>Owner: {owner}</div>
                         )}
                     </div>
 
                     {/* Second Row */}
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                        <div style={{ flexBasis: '50%' }}>Rate: {rate} Final price: {finalPrice}</div>
-                        <div style={{ flexBasis: '50%' }}>Date of reservation: {dateOfReservation}</div>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                        <div style={{flexBasis: '50%'}}>Rate: {rate} Final price: {finalPrice}</div>
+                        <div style={{flexBasis: '50%'}}>Date of reservation: {dateOfReservation}</div>
                     </div>
 
                     {/* Third Row */}
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                        <div style={{ flexBasis: '50%' }}>Damage at time of rent: {prevDamage}</div>
-                        <div style={{ flexBasis: '50%' }}>Additional damage: {additionalDamage}</div>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                        <div style={{flexBasis: '50%'}}>Damage at time of rent: {prevDamage}</div>
+                        <div style={{flexBasis: '50%'}}>Additional damage: {additionalDamage}</div>
+                    </div>
+                    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
+                        <div style={{flexBasis: '50%'}}></div>
+                        {isClient ? <Button variant={'secondary'} onClick={handleCancelButton}>Cancel Booking</Button> : null}
+                        <ToastContainer/>
                     </div>
                 </div>
             </div>
