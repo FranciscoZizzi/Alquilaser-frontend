8ec48d9253648e73cfce42486e552a15441f7aeb
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-07-09 18:36:07 -0300

    added showQR button

--- src/components/listing/Listing.tsx
+++ src/components/listing/Listing.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import EditListingPopUp from "../popUp/EditListingPopUp";
 import {Link, useNavigate} from "react-router-dom";
 import {BASE_URL} from "../../utils/constants";
 import listing from "../../pages/listing";
+import {QRCode} from "react-qrcode-logo";
+import Popup from "reactjs-popup";
+import Button from "../button/Button";
 
 const Listing = ({showEditButton, listing_id, image, title, price, availability, description}:{
     showEditButton: boolean;
@@ -13,6 +16,8 @@ const Listing = ({showEditButton, listing_id, image, title, price, availability,
     availability: string,
     description?: string
 }) => {
+    const [showQRPopup, setShowQRPopup] = useState(false);
+
     let navigate = useNavigate()
     const handleClick = async () => {
         navigate('/listing/'+ listing_id)
@@ -36,44 +41,83 @@ const Listing = ({showEditButton, listing_id, image, title, price, availability,
                 }} src={image? image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9zcd-dwhaQPEzwe_fWYDMtVVze35Ad5nMHkSk7nxMpBrOtH3_C0wVTz_z6qkVjtYdydw&usqp=CAU"} alt="Product picture"/>
             </div>
             <div style={{
-                width: "auto",
-                maxWidth: "280px" ,
+                width: "100%",
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: 'center'
-            }}> <span onClick={handleClick} style={{ cursor: "pointer"}}>
-                    <h2 style={{
-                        marginBottom: 0,
-                        padding: 5,
-                        fontSize:30
-                    }}>{title}</h2>
-                </span>
-                <div style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                }}>
-                    <p style={{
-                        fontSize:24
-                    }}>
-                        Price: ${price}/day
-                    </p>
+            }}>
+                <div>
+                    <span onClick={handleClick} style={{ cursor: "pointer"}}>
+                        <h2 style={{
+                            marginBottom: 0,
+                            padding: 5,
+                            fontSize:30
+                        }}>{title}</h2>
+                    </span>
+                </div>
+                <div style={{display:"flex", width:"100%", justifyContent:"space-between"}}>
                     <div style={{
-                        justifyContent: "space-between",
-                        marginLeft:"100%",
-                        paddingBottom: 20
+                        width:"100%",
+                        display: "flex",
+                        flexDirection: "column",
                     }}>
                         <p style={{
-                            fontSize:20,
+                            padding:0,
+                            marginBottom:0,
+                            fontSize: 24,
+                        }}>
+                            Price: ${price}/day
+                        </p>
+                        <p style={{
+                            padding:0,
+                            margin:0,
+                            fontSize: 24,
                         }}>
                             Status: {availability}
                         </p>
-                        <div>
-                            {showEditButton ? <EditListingPopUp listingId={listing_id} title={title} availability={availability}
-                                                                description={description ? description : "No description"} rate={price}/> : null
-                            }
+                    </div>
+                        <div style={{
+                            width:"auto",
+                            display: "flex",
+                            flexDirection: "column",
+                            paddingBottom: 20
+                        }}>
+                            <div>
+                                {showEditButton ?
+                                    <div style={{display: "flex", flexDirection: "column", gap:3}}>
+                                        <div>
+                                            <EditListingPopUp listingId={listing_id} title={title}
+                                                              availability={availability}
+                                                              description={description ? description : "No description"}
+                                                              rate={price}/>
+                                        </div>
+                                        <div>
+                                            <Button variant='secondary' onClick={() => setShowQRPopup(true)}>Show QR
+                                                code</Button>
+                                        </div>
+                                        <Popup
+                                            open={showQRPopup}
+                                            modal
+                                            nested
+                                            contentStyle={{
+                                                borderRadius: '10px',
+                                            }}
+                                            onClose={() => setShowQRPopup(false)}
+                                        >
+                                            <div className="modal">
+                                                <button className="close" onClick={() => setShowQRPopup(false)}>
+                                                    &times;
+                                                </button>
+                                                <h1 style={{textAlign: "center"}}>QR Code</h1>
+                                                <div style={{display: 'flex', justifyContent: 'center'}}>
+                                                    <QRCode value={`http://localhost:3002/listing/info/${listing_id}`}/>
+                                                </div>
+                                            </div>
+                                        </Popup>
+                                    </div>
+                                    : null
+                                }
                         </div>
-
                     </div>
                 </div>
             </div>
